0d927a282618dae8858c8824c942470c64f8ca2f
danielpclin <daniel.pclin@gmail.com>
2022-12-29 03:12:04 +0800

    update

--- shader/texture.fs.glsl
+++ shader/texture.fs.glsl
@@ -56,52 +56,39 @@ void main(void)
 
 
     // ambient
-    vec3 ambient = material.ambient * textureColor.rgb;
+    vec3 ambient = material.ambient * textureColor.rgb * light.ambient;
 
     // diffuse
     vec3 lightDirection = normalize(light.position - position);
-    vec3 diffuse = max(dot(normalizedNormal, lightDirection), 0.0) * textureColor.rgb * material.diffuse;
+    vec3 diffuse = max(dot(normalizedNormal, lightDirection), 0.0) * textureColor.rgb * material.diffuse * light.diffuse;
 
     // specular
     vec3 viewDirection = normalize(cameraPosition - position);
     vec3 halfwayDirection = normalize(lightDirection + viewDirection);
-    vec3 specular = pow(max(dot(normalizedNormal, halfwayDirection), 0.0), material.shininess) * material.specular;
+    vec3 specular = pow(max(dot(normalizedNormal, halfwayDirection), 0.0), material.shininess) * material.specular * light.specular;
 
     // directional light shadow
-    float bias = max(0.07 * (1.0 - dot(normalizedNormal, lightDirection)), 0.005);
+    float bias = max(0.06 * (1.0 - dot(normalizedNormal, lightDirection)), 0.005);
 
     float shadow = 0.0;
-//    if (texture(shadowMap, shadowPosition.xy).x < shadowPosition.z - bias){
-//        vec2 texelSize = 1.0 / textureSize(shadowMap, 0);
-//        for(int x = -2; x <= 2; ++x) {
-//            for(int y = -2; y <= 2; ++y) {
-//                float pcfDepth = texture(shadowMap, shadowPosition.xy + vec2(x, y) * texelSize).x;
-//                shadow += shadowPosition.z - bias > pcfDepth ? 1.0 : 0.0;
-//            }
-//        }
-//        shadow /= 25.0;
-//    } else {
-//        float closetDepth = texture(shadowMap, shadowPosition.xy).x;
-//        shadow = shadowPosition.z - bias > closetDepth ? 0.8 : 0.0;
-//    }
     vec2 texelSize = 1.0 / textureSize(shadowMap, 0);
-    for(int x = -2; x <= 2; ++x) {
-        for(int y = -2; y <= 2; ++y) {
+    for(int x = -1; x <= 1; ++x) {
+        for(int y = -1; y <= 1; ++y) {
             float pcfDepth = texture(shadowMap, shadowPosition.xy + vec2(x, y) * texelSize).x;
-            shadow += shadowPosition.z - bias > pcfDepth ? 0.8 : 0.0;
+            shadow += shadowPosition.z - bias > pcfDepth ? 1.0 : 0.0;
         }
     }
-    shadow /= 25.0;
+    shadow /= 9.0;
     shadow = 1 - shadow;
 
     color = vec4(textureColor.rgb * material.diffuse, 1.0);
 
     if (config.blinnPhong) {
-        color = vec4((ambient * light.ambient + diffuse * light.diffuse + specular * light.specular), 1.0);
+        color = vec4((ambient + diffuse + specular), 1.0);
     }
 
     if (config.directionalLightShadow) {
-        color = vec4(shadow * (ambient * light.ambient + diffuse * light.diffuse + specular * light.specular), 1.0);
+        color = vec4((ambient + shadow * (diffuse + specular)), 1.0);
     }
 
     if (config.bloom) {
